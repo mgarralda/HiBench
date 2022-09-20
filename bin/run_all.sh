e66d70f58397aafdb080f0f9dff3ad9e0a513477
Mariano Garralda <mgarralda68@gmail.com>
2022-09-20 08:10:43 +0200

    Add files via upload

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -20,92 +20,104 @@ root_dir=`cd "${current_dir}/.."; pwd`
 
 . ${root_dir}/bin/functions/color.sh
 
+prepare_data=true
+
 for benchmark in `cat $root_dir/conf/benchmarks.lst`; do
-    if [[ $benchmark == \#* ]]; then
-        continue
+	if [[ $benchmark == \#* ]]; then
+		continue
     fi
-
-    echo -e "${UYellow}${BYellow}Prepare ${Yellow}${UYellow}${benchmark} ${BYellow}...${Color_Off}"
+	
+	# Must be the first line
+	if [[ $benchmark == \skip.prepare.data ]]; then
+		prepare_data=false
+		echo -e "${UYellow}${BYellow}*** Skipping data preparation for workloads ***${BYellow}${Color_Off}"
+		continue
+	fi
+	
     benchmark="${benchmark/.//}"
-
     WORKLOAD=$root_dir/bin/workloads/${benchmark}
-    echo -e "${BCyan}Exec script: ${Cyan}${WORKLOAD}/prepare/prepare.sh${Color_Off}"
-    "${WORKLOAD}/prepare/prepare.sh"
-
-    result=$?
-    if [ $result -ne 0 ]
-    then
-    echo "ERROR: ${benchmark} prepare failed!"
-        exit $result
-    fi
-
+	
+	if $prepare_data; then
+		echo -e "${UYellow}${BYellow}Prepare ${Yellow}${UYellow}${benchmark} ${BYellow}...${Color_Off}"
+		echo -e "${BCyan}Exec script: ${Cyan}${WORKLOAD}/prepare/prepare.sh${Color_Off}"
+		"${WORKLOAD}/prepare/prepare.sh"
+	
+		result=$?
+		if [ $result -ne 0 ]
+		then
+			echo "ERROR: ${benchmark} prepare failed!"
+				exit $result
+		fi
+	fi
+	
     for framework in `cat $root_dir/conf/frameworks.lst`; do
-    if [[ $framework == \#* ]]; then
-        continue
-    fi
+		if [[ $framework == \#* ]]; then
+			continue
+		fi
 
-    if [ $benchmark == "micro/dfsioe" ] && [ $framework == "spark" ]; then
-        continue
-    fi
-    if [ $benchmark == "micro/repartition" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "websearch/nutchindexing" ] && [ $framework == "spark" ]; then
-        continue
-    fi
-    if [ $benchmark == "graph/nweight" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "graph/pagerank" ] && [ $framework == "hadoop" ]; then
-	    continue
-	  fi
-    if [ $benchmark == "ml/lr" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/als" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/svm" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/pca" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/gbt" ] && [ $framework == "hadoop" ]; then
-         continue
-    fi
-    if [ $benchmark == "ml/rf" ] && [ $framework == "hadoop" ]; then
-          continue
-    fi  
-    if [ $benchmark == "ml/svd" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi      
-    if [ $benchmark == "ml/linear" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/lda" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/gmm" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/correlation" ] && [ $framework == "hadoop" ]; then
-        continue
-    fi
-    if [ $benchmark == "ml/summarizer" ] && [ $framework == "hadoop" ]; then
-         continue
-    fi
+		if [ $benchmark == "micro/dfsioe" ] && [ $framework == "spark" ]; then
+			continue
+		fi
+		if [ $benchmark == "micro/repartition" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "websearch/nutchindexing" ] && [ $framework == "spark" ]; then
+			continue
+		fi
+		if [ $benchmark == "graph/nweight" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "graph/pagerank" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/lr" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/als" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/svm" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/pca" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/gbt" ] && [ $framework == "hadoop" ]; then
+			 continue
+		fi
+		if [ $benchmark == "ml/rf" ] && [ $framework == "hadoop" ]; then
+			  continue
+		fi  
+		if [ $benchmark == "ml/svd" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi      
+		if [ $benchmark == "ml/linear" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/lda" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/gmm" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/correlation" ] && [ $framework == "hadoop" ]; then
+			continue
+		fi
+		if [ $benchmark == "ml/summarizer" ] && [ $framework == "hadoop" ]; then
+			 continue
+		fi
 
-    echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/${framework}${Color_Off}"
-    echo -e "${BCyan}Exec script: ${Cyan}$WORKLOAD/${framework}/run.sh${Color_Off}"
-    $WORKLOAD/${framework}/run.sh
+		echo -e "${UYellow}${BYellow}Run ${Yellow}${UYellow}${benchmark}/${framework}${Color_Off}"
+		echo -e "${BCyan}Exec script: ${Cyan}$WORKLOAD/${framework}/run.sh${Color_Off}"
+		$WORKLOAD/${framework}/run.sh
+		echo -e "$WORKLOAD/${framework}/run.sh"
 
-    result=$?
-    if [ $result -ne 0 ]
-    then
-        echo -e "${On_IRed}ERROR: ${benchmark}/${framework} failed to run successfully.${Color_Off}"
-            exit $result
-    fi
+		result=$?
+		if [ $result -ne 0 ]
+		then
+			echo -e "${On_IRed}ERROR: ${benchmark}/${framework} failed to run successfully.${Color_Off}"
+				exit $result
+		fi
     done
 done
 
